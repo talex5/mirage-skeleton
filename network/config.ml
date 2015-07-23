78717f6d579c1c0db2209d9b522fcd9b46cb58b9
Thomas Leonard <talex5@gmail.com>
2015-07-23 15:13:19 +0100

    Send lots of ! over tap0

--- network/config.ml
+++ network/config.ml
@@ -1,27 +1,12 @@
 open Mirage
 
-let main = foreign "Unikernel.Main" (console @-> stackv4 @-> job)
-
-let net =
-  try match Sys.getenv "NET" with
-    | "direct" -> `Direct
-    | "socket" -> `Socket
-    | _        -> `Direct
-  with Not_found -> `Direct
-
-let dhcp =
-  try match Sys.getenv "ADDR" with
-    | "dhcp"   -> `Dhcp
-    | "static" -> `Static
-  with Not_found -> `Dhcp
-
-let stack console =
-  match net, dhcp with
-  | `Direct, `Dhcp   -> direct_stackv4_with_dhcp console tap0
-  | `Direct, `Static -> direct_stackv4_with_default_ipv4 console tap0
-  | `Socket, _       -> socket_stackv4 console [Ipaddr.V4.any]
+let main =
+  foreign
+    ~libraries:["tcpip"; "mirage-clock-unix"; "tcpip.stack-direct"]
+    ~packages:["tcpip"]
+    "Unikernel.Main" (console @-> network @-> job)
 
 let () =
   register "network" [
-    main $ default_console $ stack default_console
+    main $ default_console $ tap0
   ]
--- network/unikernel.ml
+++ network/unikernel.ml
@@ -5,27 +5,58 @@ let green fmt  = Printf.sprintf ("\027[32m"^^fmt^^"\027[m")
 let yellow fmt = Printf.sprintf ("\027[33m"^^fmt^^"\027[m")
 let blue fmt   = Printf.sprintf ("\027[36m"^^fmt^^"\027[m")
 
-module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
+let (>>!=) x f =
+  x >>= function
+  | `Ok y -> f y
+  | `Eof -> failwith "EOF"
+  | `Error _ -> failwith "Error"
 
-  let start c s =
-    S.listen_tcpv4 s ~port:80 (fun flow ->
+module Main (C: V1_LWT.CONSOLE) (Netif : V1_LWT.NETWORK) = struct
+  module Stackv41_E = Ethif.Make(Netif)
+  module Stackv41_I = Ipv4.Make(Stackv41_E)(Clock)(OS.Time)
+  module Stackv41_U = Udp.Make (Stackv41_I)
+  module Stackv41_T = Tcp.Flow.Make(Stackv41_I)(OS.Time)(Clock)(Random)
+  module S = Tcpip_stack_direct.Make(C)(OS.Time)(Random)(Netif)(Stackv41_E)(Stackv41_I)(Stackv41_U)(Stackv41_T)
+
+  let stackv41 console interface =
+    let config = {
+      V1_LWT.name = "stackv41";
+      console; interface;
+      mode = `IPv4 (Ipaddr.V4.of_string_exn "10.0.0.2", Ipaddr.V4.of_string_exn "255.255.255.0", [Ipaddr.V4.of_string_exn "10.0.0.1"]);
+    } in
+    Stackv41_E.connect interface >>= function
+    | `Error _ -> fail (Failure "Stackv41_E")
+    | `Ok ethif ->
+    Stackv41_I.connect ethif >>= function
+    | `Error _ -> fail (Failure "Stackv41_I")
+    | `Ok ipv4 ->
+    Stackv41_U.connect ipv4 >>= function
+    | `Error _ -> fail (Failure "Stackv41_U")
+    | `Ok udpv4 ->
+    Stackv41_T.connect ipv4 >>= function
+    | `Error _ -> fail (Failure "Stackv41_T")
+    | `Ok tcpv4 ->
+    S.connect config ethif ipv4 udpv4 tcpv4
+
+  let buffer = Io_page.get 1 |> Io_page.to_cstruct
+  let payload = Cstruct.sub buffer 0 1400
+  let () =
+    Cstruct.blit_from_string (String.make 1400 '!') 0 payload 0 1400
+
+  let start c interface =
+    stackv41 c interface >>!= fun s ->
+    S.listen_tcpv4 s ~port:8080 (fun flow ->
         let dst, dst_port = S.TCPV4.get_dest flow in
         C.log_s c (green "new tcp connection from %s %d"
                      (Ipaddr.V4.to_string dst) dst_port)
         >>= fun () ->
-        S.TCPV4.read flow
-        >>= function
-        | `Ok b ->
-          C.log_s c
-            (yellow "read: %d\n%s"
-               (Cstruct.len b) (Cstruct.to_string b)
-            )
-          >>= fun () ->
-          S.TCPV4.close flow
-
-        | `Eof -> C.log_s c (red "read: eof")
-
-        | `Error e -> C.log_s c (red "read: error")
+        let rec aux = function
+          | 0 -> return ()
+          | n ->
+              S.TCPV4.write flow payload >>!= fun () ->
+              aux (n - 1) in
+        aux 100 >>= fun () ->
+        S.TCPV4.close flow
       );
 
     S.listen s
