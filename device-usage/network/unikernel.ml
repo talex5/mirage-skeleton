e2df0a7eefe2b97df5aa17f6fd7ddcf3f82e7e07
Thomas Leonard <talex5@gmail.com>
2020-08-23 16:01:42 +0100

    WIP: Attempt to reproduce crash with solo5
    From https://github.com/mirage/qubes-mirage-firewall/pull/116#issuecomment-678768951
    
    This works OK for me when compiled using the `Dockerfile.fedora`, but
    crashes when compiled on native Qubes, with the same versions of Fedora,
    GCC, OCaml and the opam libraries.

--- device-usage/network/unikernel.ml
+++ device-usage/network/unikernel.ml
@@ -1,8 +1,13 @@
 open Lwt.Infix
+open Qubes
+module Flow = Qubes.RExec.Flow
 
 module Main (S: Mirage_stack.V4) = struct
 
+  let handler ~user:_ cmd flow = Lwt.return 0
+
   let start s =
+    (* Start qrexec agent, GUI agent and QubesDB agent in parallel *)
     let port = Key_gen.port () in
     S.listen_tcpv4 s ~port (fun flow ->
         let dst, dst_port = S.TCPV4.dst flow in
@@ -15,6 +20,12 @@ module Main (S: Mirage_stack.V4) = struct
           Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
           S.TCPV4.close flow
       );
+
+    let qrexec = RExec.connect ~domid:0 () in
+    qrexec >>= fun qrexec ->
+    (* OS.Time.sleep_ns (0.0 *. 1e9 |> Int64.of_float) >>= fun () -> *)
+    let agent_listener = RExec.listen qrexec handler in
+    Logs.info (fun f -> f "ready to listen for new packets");
 
     S.listen s
 
